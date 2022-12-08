6e8c08e32db59e9ac447bf49f828f2c7767fa0bf
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-12-07 21:13:52 -0400

    feat: icons

---
+++ src/pages/_document.js
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function MyDocument() {
+  return (
+    <Html className="scroll-smooth">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Taviraj&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
