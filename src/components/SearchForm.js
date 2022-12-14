a2b7d5f90ef3edf8dacfb6152e06b9b75829191a
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-12-14 15:24:09 -0400

    fix: search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,7 +2,7 @@ import Search from './Icons/SearchIcon';
 
 export default function SearchForm() {
   return (
-    <div className="flex">
+    <div className="flex justify-end">
       <input
         id="input"
         className="transition-[width] duration-[1500ms] text-sm focus:pl-3 focus:outline-none focus:p-1 w-0 focus:w-full border-y border-l bg-white border-slate-200"
