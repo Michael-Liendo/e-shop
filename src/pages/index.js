9f74785a0800550fe6146b2f424b70fa6efa6875
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-12-16 13:32:22 -0400

    feat: categories filter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,63 @@
+import { useState } from 'react';
 import Navbar from '../components/Navbar';
 import ProductCard from '../components/ProductCard';
 
-export default function Home({ products }) {
+export default function Home({ products, categories }) {
+  const [showCategories, setShowCategories] = useState('all');
+
+  if (showCategories !== 'all') {
+    products = products.filter(
+      (product) => product.category.name.toLowerCase() === showCategories
+    );
+  }
+
   return (
     <div>
       <Navbar />
-      <div className="ml-80 grid grid-cols-3 gap-4">
-        {products
-          ? products.map((product) => (
-              <ProductCard product={product} key={product.id} />
-            ))
-          : null}
+      <div className="flex pt-20">
+        <aside className="overflow-y-scroll fixed h-[89vh] w-[25%]">
+          <p className="ml-10 text-xl font-semibold">Categories</p>
+          <h2
+            onClick={() => setShowCategories('all')}
+            className="ml-12 cursor-pointer text-lg"
+          >
+            All
+          </h2>
+          {categories
+            ? categories.map((category) => (
+                <h2
+                  onClick={() => setShowCategories(category.name.toLowerCase())}
+                  className="ml-12 cursor-pointer text-lg"
+                  key={category.id}
+                >
+                  {category.name}
+                </h2>
+              ))
+            : null}
+        </aside>
+        <main className="mt-5 ml-[32%] grid sm:grid-cols-2 lg:grid-cols-3 gap-10">
+          {products
+            ? products.map((product) => (
+                <ProductCard product={product} key={product.id} />
+              ))
+            : null}
+        </main>
       </div>
     </div>
   );
 }
 
 export async function getServerSideProps() {
-  const request = await fetch('https://api.escuelajs.co/api/v1/products');
-  const products = await request.json();
+  const categoriesRequest = await fetch(
+    'https://api.escuelajs.co/api/v1/categories'
+  );
+
+  const categories = await categoriesRequest.json();
+  const productsRequest = await fetch(
+    'https://api.escuelajs.co/api/v1/products'
+  );
+  const products = await productsRequest.json();
   return {
-    props: { products },
+    props: { products, categories },
   };
 }
