0f5edafd08d52735e56f6d341b65a344fb907947
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-12-20 11:46:31 -0400

    style: change navbar and search form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,11 @@
-import Link from 'next/link';
 import SearchForm from './SearchForm';
-
-function NavLink({ children, href }) {
-  return (
-    <Link
-      title={children}
-      className="transition ease-in mx-4 hover:text-teal-400 duration-150"
-      href={href}
-    >
-      {children}
-    </Link>
-  );
-}
 
 export default function Navbar() {
   return (
     <div className="z-50 shadow-md bg-white flex fixed w-[100%] justify-between text-xl pt-6 pb-3 px-7">
-      <h1>LOGO</h1>
-      <div className="absolute left-[40%]">
-        <NavLink href="#">Ropa</NavLink>
-        <NavLink href="#">Electrónicos</NavLink>
-        <NavLink href="#">Zapatos</NavLink>
-      </div>
-      <div className="">
-        <SearchForm />
+      <SearchForm />
+      <div className="absolute left-[50%]">
+        <h1>LOGO</h1>
       </div>
     </div>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,14 +2,18 @@ import Search from './Icons/SearchIcon';
 
 export default function SearchForm() {
   return (
-    <div className="flex justify-end">
-      <input
-        id="input"
-        className="transition-[width] duration-[1500ms] text-sm focus:pl-3 focus:outline-none focus:p-1 w-0 focus:w-full border-y border-l bg-white border-slate-200"
-      />
-      <label className="border-y border-r p-1 border-slate-200" htmlFor="input">
+    <div className="flex justify-start">
+      <label
+        className="rounded-l-sm border-y border-l p-1 border-slate-200"
+        htmlFor="input"
+      >
         <Search />
       </label>
+      <input
+        placeholder="Search for items"
+        id="input"
+        className="rounded-r-sm transition-[width] duration-700 text-sm focus:pl-3 focus:outline-none focus:p-1 w-0 focus:w-full border-y border-r bg-white border-slate-200"
+      />
     </div>
   );
 }
