d112266d001fe4198c2f38c1016b221f0222e025
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-01-17 09:28:08 -0400

    chore: change header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import SearchForm from './SearchForm';
 import Cart from './Cart';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
     <div className="z-50 shadow-md bg-white flex fixed w-[100%] justify-between text-xl pt-6 pb-3 px-7">
       <SearchForm />
       <div className="absolute left-[48%]">
-        <h1>LOGO</h1>
+        <Link href="/">
+          <h1>E-commerce</h1>
+        </Link>
       </div>
       <Cart />
     </div>
