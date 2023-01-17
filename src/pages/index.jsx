65a0406a3f25c21784f29bc86ac9a34fef69d153
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-01-17 08:58:15 -0400

    chore: add title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import cn from 'classnames';
 import Navbar from '../components/Navbar';
 import ProductCard from '../components/ProductCard';
 import { useShopStore } from '../store/ShopStore';
+import Head from 'next/head';
 
 export default function Home({ products, categories }) {
   const { searchForm } = useShopStore();
@@ -37,76 +38,83 @@ export default function Home({ products, categories }) {
   }
 
   return (
-    <div>
-      <Navbar />
-      <div className="flex pt-20">
-        <aside className="hidden sm:block overflow-y-scroll fixed h-[89vh] w-[25%]">
-          <h3 className="ml-10 text-xl font-semibold">Categories</h3>
-          <ul>
-            <li
-              onClick={() => setShowCategories('all')}
-              className={cn('ml-12 cursor-pointer text-lg', {
-                'font-semibold': showCategories === 'all',
-              })}
-            >
-              All
-            </li>
-            {categories.length ? (
-              categories.map((category) => (
-                <li
-                  onClick={() => setShowCategories(category.name.toLowerCase())}
-                  className={cn('ml-12 cursor-pointer text-lg', {
-                    'font-semibold':
-                      showCategories === category.name.toLowerCase(),
-                  })}
-                  key={category.id}
-                >
-                  {category.name}
-                </li>
+    <>
+      <Head>
+        <title>E-Commerce</title>
+      </Head>
+      <div>
+        <Navbar />
+        <div className="flex pt-20">
+          <aside className="hidden sm:block overflow-y-scroll fixed h-[89vh] w-[25%]">
+            <h3 className="ml-10 text-xl font-semibold">Categories</h3>
+            <ul>
+              <li
+                onClick={() => setShowCategories('all')}
+                className={cn('ml-12 cursor-pointer text-lg', {
+                  'font-semibold': showCategories === 'all',
+                })}
+              >
+                All
+              </li>
+              {categories.length ? (
+                categories.map((category) => (
+                  <li
+                    onClick={() =>
+                      setShowCategories(category.name.toLowerCase())
+                    }
+                    className={cn('ml-12 cursor-pointer text-lg', {
+                      'font-semibold':
+                        showCategories === category.name.toLowerCase(),
+                    })}
+                    key={category.id}
+                  >
+                    {category.name}
+                  </li>
+                ))
+              ) : (
+                <span>No hay más categorías</span>
+              )}
+            </ul>
+            <h3 className="ml-10 mt-6 text-xl font-semibold">Sort By</h3>
+            <ul>
+              <li
+                className={cn('ml-12 cursor-pointer text-lg', {
+                  'font-semibold': sortBy === 'recent',
+                })}
+                onClick={() => setSortBy('recent')}
+              >
+                Recent
+              </li>
+              <li
+                className={cn('ml-12 cursor-pointer text-lg', {
+                  'font-semibold': sortBy === 'lowest-price',
+                })}
+                onClick={() => setSortBy('lowest-price')}
+              >
+                Lowest price
+              </li>
+              <li
+                className={cn('ml-12 cursor-pointer text-lg', {
+                  'font-semibold': sortBy === 'highest-price',
+                })}
+                onClick={() => setSortBy('highest-price')}
+              >
+                Highest price
+              </li>
+            </ul>
+          </aside>
+          <main className="mt-5 mx-auto sm:ml-[32%] grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-10">
+            {products.length ? (
+              products.map((product, index) => (
+                <ProductCard product={product} key={index} />
               ))
             ) : (
-              <span>No hay más categorías</span>
+              <h1>No hay products</h1>
             )}
-          </ul>
-          <h3 className="ml-10 mt-6 text-xl font-semibold">Sort By</h3>
-          <ul>
-            <li
-              className={cn('ml-12 cursor-pointer text-lg', {
-                'font-semibold': sortBy === 'recent',
-              })}
-              onClick={() => setSortBy('recent')}
-            >
-              Recent
-            </li>
-            <li
-              className={cn('ml-12 cursor-pointer text-lg', {
-                'font-semibold': sortBy === 'lowest-price',
-              })}
-              onClick={() => setSortBy('lowest-price')}
-            >
-              Lowest price
-            </li>
-            <li
-              className={cn('ml-12 cursor-pointer text-lg', {
-                'font-semibold': sortBy === 'highest-price',
-              })}
-              onClick={() => setSortBy('highest-price')}
-            >
-              Highest price
-            </li>
-          </ul>
-        </aside>
-        <main className="mt-5 mx-auto sm:ml-[32%] grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-10">
-          {products.length ? (
-            products.map((product, index) => (
-              <ProductCard product={product} key={index} />
-            ))
-          ) : (
-            <h1>No hay products</h1>
-          )}
-        </main>
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
